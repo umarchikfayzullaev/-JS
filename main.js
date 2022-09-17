5a40c6c9cc3f4680428f0fc3ffa09bbffe6bcef6
Umarchik <umarjonfayzullaev2004@gmail.com>
2022-09-17 15:59:25 +0500

    problem 3 and 4

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ num = (num--, num *= 3, num = minusEight(num), num -=4);
 console.log(num);
 // ansver is 24
 
+
 let a = 0;
 for(let i = 0; i < 5; i++) {
     a++
@@ -17,9 +18,20 @@ for(let i = 0; i < 5; i++) {
 console.log(5);
 // answer is 5
 
+
 const sym = Symbol("key");
 const javascript_tests = Symbol.keyFor(sym);
-
 console.log(javascript_tests);
-
 // ansver (undefined)
+
+
+// const arr = [NaN, 10, false];
+// console.log(Array.from(arr, x => x +x));
+// answer ([Nan, 20, 0])
+
+// const arr = [2, 5, 7, 15];
+// const func = (element) => {
+//   if (element > 6) return true;
+// }
+// console.log(arr.find(func));
+// answer (7)
